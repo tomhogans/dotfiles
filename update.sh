c3f604718a22d3b4120d6490886106c51d14b75e
Tom Hogans <tomhsx@gmail.com>
2017-10-23 03:19:03 -0400

    Remove need to clone repo

--- update.sh
+++ update.sh
@@ -5,11 +5,11 @@ PLATFORM_NAME=`uname`
 if [ "$PLATFORM_NAME" == "Darwin" ]; then
     echo "Detected MacOS install, proceeding with MacOS setup..."
     #bash setup_macos.bash
-    sh -c "$(https://raw.githubusercontent.com/tomhogans/dotfiles/master/setup_macos.bash)"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/tomhogans/dotfiles/master/setup_macos.bash)"
 
 else
     echo "Detected Linux install, proceeding with Ubuntu setup..."
-    sh -c "$(https://github.com/tomhogans/dotfiles/blob/master/setup_ubuntu.bash)"
+    sh -c "$(curl -fsSL https://github.com/tomhogans/dotfiles/blob/master/setup_ubuntu.bash)"
 fi
 
 # pip libraries (ipython, flake8 etc)
