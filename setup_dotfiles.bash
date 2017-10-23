6bab7f285fe3122da38b21be8202a78dcbc76791
Tom Hogans <tomhsx@gmail.com>
2017-10-23 03:08:42 -0400

    Use http locations for config files

--- setup_dotfiles.bash
+++ setup_dotfiles.bash
@@ -1,15 +1,15 @@
-echo "Setting up vim and plugins..."
-mkdir -p ~/.vim
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cp config/vimrc ~/.vimrc
-
 echo "Configuring git..."
 git config --global alias.st status
 git config --global alias.br branch
 git config --global alias.co checkout
 
+echo "Setting up vim and plugins..."
+mkdir -p ~/.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+curl https://raw.githubusercontent.com/tomhogans/dotfiles/master/config/vimrc > ~/.vimrc
+
 echo "Installing and configuring oh-my-zsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-cp config/zshrc ~/.zshrc
+curl https://github.com/tomhogans/dotfiles/blob/master/config/zshrc > ~/.zshrc
 
 echo "Dotfiles configuration complete."
