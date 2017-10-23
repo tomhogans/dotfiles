7b87b4a7a7029be802ef91bf5a4bc69fa1ada611
Tom Hogans <tomhsx@gmail.com>
2017-10-23 03:15:28 -0400

    Use github content urls

--- setup_dotfiles.bash
+++ setup_dotfiles.bash
@@ -6,10 +6,10 @@ git config --global alias.co checkout
 echo "Setting up vim and plugins..."
 mkdir -p ~/.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-curl https://raw.githubusercontent.com/tomhogans/dotfiles/master/config/vimrc > ~/.vimrc
+curl -fsSL https://raw.githubusercontent.com/tomhogans/dotfiles/master/config/vimrc > ~/.vimrc
 
 echo "Installing and configuring oh-my-zsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-curl https://github.com/tomhogans/dotfiles/blob/master/config/zshrc > ~/.zshrc
+curl -fsSL https://github.com/tomhogans/dotfiles/blob/master/config/zshrc > ~/.zshrc
 
 echo "Dotfiles configuration complete."
--- update.sh
+++ update.sh
@@ -4,14 +4,17 @@ PLATFORM_NAME=`uname`
 
 if [ "$PLATFORM_NAME" == "Darwin" ]; then
     echo "Detected MacOS install, proceeding with MacOS setup..."
-    bash setup_macos.bash
+    #bash setup_macos.bash
+    sh -c "$(https://raw.githubusercontent.com/tomhogans/dotfiles/master/setup_macos.bash)"
+
 else
     echo "Detected Linux install, proceeding with Ubuntu setup..."
-    bash setup_ubuntu.bash
+    sh -c "$(https://github.com/tomhogans/dotfiles/blob/master/setup_ubuntu.bash)"
 fi
 
 # pip libraries (ipython, flake8 etc)
 pip3 install -r requirements.txt
 
 # dotfiles (gitconfig, vim, tmux)
-sh setup_dotfiles.bash
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/tomhogans/dotfiles/master/setup_dotfiles.bash)"
+
